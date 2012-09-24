b1a5e28b0032a343568a2a6026124f43fbb126fb
Lynn Wallenstein <lynn.wallenstein@gmail.com>
2012-09-24 10:42:31 -0400

    Text cleanup for chrome frame

--- js/plugins.js
+++ js/plugins.js
@@ -1,45 +1,17 @@
-// usage: log('inside coolFunc', this, arguments);
-// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var noop = function noop() {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = window.console || {};
 
-window.log = function f() {
-    log.history = log.history || [];
-    log.history.push(arguments);
-    if (this.console) {
-        var args = arguments;
-        var newarr;
-
-        try {
-            args.callee = f.caller;
-        } catch(e) {
-
-        }
-
-        newarr = [].slice.call(args);
-
-        if (typeof console.log === 'object') {
-            log.apply.call(console.log, console, newarr);
-        } else {
-            console.log.apply(console, newarr);
-        }
-    }
-};
-
-// make it safe to use console.log always
-
-(function(a) {
-    function b() {}
-    var c = "assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn";
-    var d;
-    for (c = c.split(","); !!(d = c.pop());) {
-        a[d] = a[d] || b;
-    }
-})(function() {
-    try {
-        console.log();
-        return window.console;
-    } catch(a) {
-        return (window.console = {});
+    while (length--) {
+        // Only stub undefined methods.
+        console[methods[length]] = console[methods[length]] || noop;
     }
 }());
-
-// place any jQuery/helper plugins in here, instead of separate, slower script files.
